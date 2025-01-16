73a6b9ec7a1fc58e58628edde72936e4279bc98b
José Carlos Castillo Villa <DevJCCastilloVilla@outlook.es>
2025-01-16 01:04:45 -0600

    se integra primer formato de excel sin tomos.

--- app/Exports/CotizacionExportExcel.php
+++ app/Exports/CotizacionExportExcel.php
@@ -27,17 +27,18 @@ class CotizacionExportExcel implements FromView
     //     ]);
     // }
 
+
     public function view(): View
     {
         return view('exports.cotizacion', [
             'detalles' => [
                 ['PDA' => '1.01', 'descripcion' => 'Desmantelamiento de puntas existentes', 'unidad' => 'Pieza', 'cantidad' => 6, 'costo_unitario' => 100, 'subtotal' => 600],
+                ['PDA' => '1.02', 'descripcion' => 'Desmantelamiento de puntas existentes', 'unidad' => 'Pieza', 'cantidad' => 6, 'costo_unitario' => 100, 'subtotal' => 600],
+                ['PDA' => '1.03', 'descripcion' => 'Desmantelamiento de puntas existentes', 'unidad' => 'Pieza', 'cantidad' => 6, 'costo_unitario' => 100, 'subtotal' => 600],
+                ['PDA' => '1.04', 'descripcion' => 'Desmantelamiento de puntas existentes', 'unidad' => 'Pieza', 'cantidad' => 6, 'costo_unitario' => 100, 'subtotal' => 600],
+                ['PDA' => '1.05', 'descripcion' => 'Desmantelamiento de puntas existentes', 'unidad' => 'Pieza', 'cantidad' => 6, 'costo_unitario' => 100, 'subtotal' => 600],
                 // Agrega más registros aquí
             ]
         ]);
     }
-
-
-
-
 }
--- resources/views/exports/cotizacion.blade.php
+++ resources/views/exports/cotizacion.blade.php
@@ -1,97 +1,124 @@
-
-<table>
+{{-- <table >
     <tr>
-        <td  style="text-align: center; height: 100px;">
+        <td   style="text-align: center; height: 100px;">
             <img class="text-align: center;" src="{{ public_path('images/EncabezadoExcel.jpg') }}" height="100" width="100">
         </td>
     </tr>
-</table>
+</table> --}}
 
 <table>
-    <tr rowspan="8" >
-        <td  style="text-align: center; font-weight: bold; font-size: 16px;">
-            INNOVACIÓN NACIONAL DE INGENIERÍA, S.A. DE C.V.
+    <tr>
+        <td colspan="5" style="text-align: justify; font-weight: bold; font-size: 16px;">
+            "GRUPO NESTLÉ MÉXICO/UNIDAD DE COMPRAS"
+        </td>
+    </tr>
+    <tr>
+        <td colspan="5" style="text-align: justify; font-weight: bold; font-size: 16px;">
+            P R E S E N T E .
+        </td>
+    </tr>
+    <!-- Fila vacía -->
+    <tr>
+        <td style="height: 20px;"></td>
+    </tr>
+    <tr>
+        <td colspan="3" style="text-align: justify; font-weight: bold; font-size: 14px; color:#008ae0 ;">
+            COT: NEME 102024/237
         </td>
     </tr>
 
+    <tr>
+        <td colspan="3" style="text-align: justify; font-weight: bold; font-size: 14px; color:red ;">
+            No. PROVEEDOR: 100613254
+        </td>
+
+        <td colspan="1" style="text-align: justify; font-weight: bold; font-size: 14px;">
+            COMPAÑÍA:
+        </td>
+
+        <td colspan="6" style="text-align: center; font-weight: bold; font-size: 14px; ">
+            INNOVACION NACIONAL DE INGENIERIA S.A. DE C.V.
+        </td>
+    </tr>
+
+    <tr>
+        <td colspan="3" style="text-align: justify; font-weight: bold; font-size: 14px; ">
+            DESCRIPCIÓN DEL
+        </td>
+
+
+
+        <td colspan="1" style="text-align: justify; font-weight: bold; font-size: 14px; ">
+            TÍTULO:
+        </td>
+
+        <td colspan="6" style="text-align: center; font-weight: bold; font-size: 14px; ">
+            INSTALACIÓN DE SISTEMA DE PARARRAYOS.
+        </td>
+    </tr>
+    <tr>
+        <td colspan="3" style="text-align: justify; font-weight: bold; font-size: 14px; ">
+            ALCANCE:
+        </td>
+    </tr>
 </table>
 
 
-{{-- <table>
-    <!-- Encabezado del documento -->
-    <tr>
-        <td rowspan="4">
-            <img src="{{ public_path('images/EncabezadoExcel.jpg') }}" height="100">
-        </td>
 
-    </tr>
+<table>
+    <thead>
+        <tr style="border: 1px solid #000; text-align: center;">
+            <th rowspan="3" style="background-color: #D9FBD9; font-weight: bold; text-align: center;  width: 100px;" >PDA</th>
+            <th rowspan="3" style="background-color: #D9FBD9; font-weight: bold; text-align: center;  width: 200px;">
+                DESCRIPCIÓN
+            </th>
+            <th colspan="4" style="background-color: #c0c0c0; font-weight: bold; text-align: center;  width: 100px;">COSTO DE
+                MATERIALES</th>
 
-    <tr>
-        <td colspan="5" style="text-align: center; font-weight: bold; font-size: 16px;">
-            INNOVACIÓN NACIONAL DE INGENIERÍA, S.A. DE C.V.
-        </td>
-    </tr>
-    <tr>
-        <td colspan="5" style="text-align: center; font-size: 12px;">
-            RIO BRAVO No. 1006, COL. CAROLINA ANAYA, XALAPA, VER. C.P. 91158
-        </td>
-    </tr>
-    <tr>
-        <td colspan="5" style="text-align: center; font-size: 12px;">
-            E-mail: inisa_2005@yahoo.com.mx | TEL./FAX: 01(228) 834-55-21
-        </td>
-    </tr>
-    <tr>
-        <td colspan="5" style="text-align: center; font-size: 12px;">
-            URGENCIAS: 044(228) 824-37-53
-        </td>
-    </tr>
-
-    <!-- Espacio entre encabezado y tabla -->
-    <tr><td colspan="6"></td></tr>
-
-    <!-- Subencabezado -->
-    <tr>
-        <td colspan="6" style="font-weight: bold;">"GRUPO NESTLÉ MÉXICO/UNIDAD DE COMPRAS"</td>
-    </tr>
-    <tr>
-        <td colspan="6" style="font-weight: bold;">P R E S E N T E.</td>
-    </tr>
-
-    <tr>
-        <td>COT:</td>
-        <td>102024/237</td>
-        <td>No. PROVEEDOR:</td>
-        <td>100613254</td>
-        <td>COMPAÑÍA:</td>
-        <td>INNOVACIÓN NACIONAL DE INGENIERÍA</td>
-    </tr>
-    <tr>
-        <td colspan="6"><b>INSTALACIÓN DE SISTEMA DE PARARRAYOS</b></td>
-    </tr>
-
-    <!-- Espacio entre encabezados y datos -->
-    <tr><td colspan="6"></td></tr>
-
-    <!-- Encabezado de la tabla -->
-    <tr style="background-color: #d9d9d9; font-weight: bold;">
-        <td>PDA</td>
-        <td>DESCRIPCIÓN</td>
-        <td>UNID</td>
-        <td>CANT.</td>
-        <td>COSTO UNITARIO</td>
-        <td>SUBTOTAL</td>
-    </tr>
-
-    <!-- Datos dinámicos -->
-    @foreach ($detalles as $detalle)
-        <tr>
-            <td>{{ $detalle['PDA'] }}</td>
-            <td>{{ $detalle['descripcion'] }}</td>
-            <td>{{ $detalle['unidad'] }}</td>
-            <td>{{ $detalle['cantidad'] }}</td>
-            <td>{{ $detalle['costo_unitario'] }}</td>
-            <td>{{ $detalle['subtotal'] }}</td>
+            <th colspan="2" style="background-color: #c0c0c0; font-weight: bold; text-align: center;  width: 100px;">COSTOS DE MANO
+                DE OBRA</th>
+            <th colspan="2" style="background-color: #c0c0c0; font-weight: bold; text-align: center;  width: 100px;">M.O./MATER.
+            </th>
         </tr>
-    @endforeach
-</table> --}}
+        <tr>
+            <th rowspan="2" style="background-color: #FFF2CC; font-weight: bold; text-align: center;  width: 100px;">UNID</th>
+            <th rowspan="2" style="background-color: #FFF2CC; font-weight: bold; text-align: center;  width: 100px;">CANT.</th>
+            <td colspan="2" style="background-color: #D9FBD9; font-weight: bold; text-align: center;  width: 100px;">PESOS M.N.</td>
+            <td colspan="2" style="background-color: #ccffff; font-weight: bold; text-align: center;  width: 100px;">PESOS M.N.</td>
+            <td colspan="2" style="background-color: #c0c0c0; font-weight: bold; text-align: center;  width: 100px;">PESOS M.N.</td>
+        </tr>
+        <tr>
+            {{-- <th colspan="4" style="background-color: #D9FBD9; font-weight: bold; text-align: center; "></th> --}}
+            <td colspan="1" style="background-color: #FFF2CC; font-weight: bold; text-align: center; width: 150px;">COSTO UNITARIO
+            </td>
+            <td colspan="1" style="background-color: #FFF2CC; font-weight: bold; text-align: center; width: 100px;">SUBTOTAL</td>
+            <td colspan="1" style="background-color: #FFF2CC; font-weight: bold; text-align: center; width: 150px;">COSTO UNITARIO
+            </td>
+            <td colspan="1" style="background-color: #FFF2CC; font-weight: bold; text-align: center; width: 100px;">SUBTOTAL</td>
+            <td colspan="2" style="background-color: #FFF2CC; font-weight: bold; text-align: center; width: 100px;">SUBTOTAL</td>
+        </tr>
+
+
+
+    </thead>
+
+    <tbody style="text-align: center;">
+        @foreach ($detalles as $detalle)
+            <tr>
+                <td style="text-align: center;" >{{ $detalle['PDA'] }}</td>
+                <td style="text-align: center;" >{{ $detalle['descripcion'] }}</td>
+                <td style="text-align: center;" >{{ $detalle['unidad'] }}</td>
+                <td style="text-align: center;" >{{ $detalle['cantidad'] }}</td>
+                <td style="text-align: center;" >$ {{ number_format($detalle['costo_unitario'], 2, '.', '') }}</td>
+                <td style="text-align: center;" >$ {{ number_format($detalle['subtotal'], 2, '.', '') }}</td>
+                <td style="text-align: center;" >$ {{ number_format($detalle['costo_unitario'], 2, '.', '') }}</td>
+                <td style="text-align: center;" >$ {{number_format( $detalle['subtotal'], 2, '.', '') }}</td>
+                <td style="text-align: center;"  colspan="2">$ {{number_format($detalle['subtotal'], 2, '.', '') }}</td>
+            </tr>
+        @endforeach
+    </tbody>
+</table>
+
+
+
+
