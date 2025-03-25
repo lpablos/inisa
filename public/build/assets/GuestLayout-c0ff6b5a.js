bc2ec3f266b007d7fffe80ff2b43690d5657558b
Luis Jorge <gevolutionxalapa@gmail.com>
2025-03-24 22:35:04 -0600

    Compilado

---
+++ public/build/assets/GuestLayout-c0ff6b5a.js
@@ -0,0 +1,1 @@
+import{j as e}from"./app-fec1f9ef.js";function m({children:s}){return e.jsx("div",{className:"min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-white",children:e.jsx("div",{className:"w-full sm:max-w-md px-6 my-auto py-4 bg-white shadow-md overflow-hidden sm:rounded-lg",children:s})})}export{m as G};
---
+++ public/build/assets/InputLabel-cb190751.js
@@ -0,0 +1,1 @@
+import{j as o}from"./app-fec1f9ef.js";function s({value:t,className:e="",children:m,...n}){return o.jsx("label",{...n,className:"block text-900 font-medium mb-2"+e,children:t||m})}export{s as I};
