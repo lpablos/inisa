c4d0e12d3aa5af959a58c2380305c55e4777546f
José Carlos Castillo Villa <DevJCCastilloVilla@outlook.es>
2025-03-25 21:09:10 -0600

    modificacion de site

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -1,23 +1,32 @@
-import { Link, Head } from '@inertiajs/react';
-import {LayoutContext, LayoutProvider} from "@/Layouts/layout/context/layoutcontext.jsx";
-import {PrimeReactProvider} from "primereact/api";
-import {Button} from "primereact/button";
-import React, {useContext} from "react";
+import { Link, Head } from "@inertiajs/react";
+import {
+    LayoutContext,
+    LayoutProvider,
+} from "@/Layouts/layout/context/layoutcontext.jsx";
+import { PrimeReactProvider } from "primereact/api";
+import { Button } from "primereact/button";
+import React, { useContext } from "react";
 
 export default function Welcome({ auth, laravelVersion, phpVersion }) {
-    const { layoutConfig} = useContext(LayoutContext);
+    const { layoutConfig } = useContext(LayoutContext);
     return (
         <>
+
             <PrimeReactProvider>
                 <LayoutProvider>
-            <Head title="Welcome" />
-            <div className="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
-                <div className="sm:fixed sm:top-0 sm:left-0 p-6">
+                    <Head title="Welcome" />
+                    <div className="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
+                        <div className="sm:fixed sm:top-0 sm:left-0 p-6">
+                            <div className="flex align-items-center">
+                                <img
+                                    src={`/images/logo.jpeg`}
+                                    width="100%"
+                                    height="100"
+                                    alt="logo"
+                                    className="mr-3"
+                                />
 
-                    <div className="flex align-items-center">
-                        <img src={`/images/logo.jpeg`} width="100%" height='100' alt="logo" className="mr-3"/>
-
-                        {/* {auth.user ? (
+                                {/* {auth.user ? (
                             <Link
                                 href={route('dashboard')}
                                 className="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
@@ -41,23 +50,50 @@ export default function Welcome({ auth, laravelVersion, phpVersion }) {
                                 </Link>
                             </>
                         )} */}
-                    </div>
-                </div>
+                            </div>
+                        </div>
+
+                        <div className="grid grid-nogutter surface-0 text-800">
+                            <div className="col-12 md:col-6 p-6 text-center md:text-left flex align-items-center ">
+                                <section className="text-left max-w-3xl mx-auto px-6 py-12">
+                                    <h1 className="text-5xl font-extrabold leading-tight mb-4 text-gray-900">
+                                        Llevamos la energía donde más la
+                                        necesitas
+                                    </h1>
 
 
-                <div className="grid grid-nogutter surface-0 text-800">
-                    <div className="col-12 md:col-6 p-6 text-center md:text-left flex align-items-center ">
-                        <section>
-                            <span className="block text-6xl font-bold mb-1">Create the dashboard</span>
-                            <div className="text-6xl text-primary font-bold mb-3">your clients deserve to see</div>
-                            <p className="mt-0 mb-4 text-700 line-height-3">Craft a visually stunning and user-centric dashboard that showcases the information your clients deserve with bold, impactful design elements.</p>
 
-                            {/* <Button label="Learn More" type="button" className="mr-3 p-button-raised" />
-                            <Button label="Live Demo" type="button" className="p-button-outlined" /> */}
-                        </section>
-                    </div>
-                    <div className="col-12 md:col-6 overflow-hidden">
-                        {/* <video
+
+
+                                    <p style={{ color: "#e37816" }}  className="text-3xl font-bold leading-snug text-indigo-600 mb-6 text-brand-orange" >
+                                        En Innovación Nacional de Ingeniería,
+                                        contamos con más de 25 años de
+                                        experiencia realizando instalaciones
+                                        eléctricas precisas, confiables y
+                                        seguras <br />
+                                        en Xalapa, Veracruz.
+                                    </p>
+
+                                    <p className="text-gray-600 text-lg">
+                                        Nuestro equipo está conformado por
+                                        personal altamente calificado y
+                                        certificado, listo para atender
+                                        cualquier necesidad en reparación,
+                                        instalación o asesoría técnica.
+                                    </p>
+
+
+
+                                    <p className="text-gray-600 text-lg">
+                                        Nos especializamos en brindarte una
+                                        atención personalizada, asegurándonos de
+                                        que tomes la mejor decisión para tu
+                                        proyecto.
+                                    </p>
+                                </section>
+                            </div>
+                            <div className="col-12 md:col-6 overflow-hidden">
+                                {/* <video
                             src={`/images/video/promo.mp4`} // Ruta del video
                             autoPlay
                             muted
@@ -66,12 +102,19 @@ export default function Welcome({ auth, laravelVersion, phpVersion }) {
                             className="absolute top-0 left-0 w-full h-full object-cover"
                             style={{ clipPath: 'polygon(8% 0, 100% 0%, 100% 100%, 0 100%)' }}
                         /> */}
-                        <img src="/images/hero/hero-1.png" alt="hero-1" className="md:ml-auto block md:h-full" style={{ clipPath: 'polygon(8% 0, 100% 0%, 100% 100%, 0 100%)' }} />
+                                <img
+                                    src="/images/hero/logo_portada.webp"
+                                    alt="hero-1"
+                                    className="md:ml-auto block md:h-full"
+                                    style={{
+                                        clipPath:
+                                            "polygon(8% 0, 100% 0%, 100% 100%, 0 100%)",
+                                    }}
+                                />
+                            </div>
+                        </div>
                     </div>
-                </div>
-
-            </div>
-            </LayoutProvider>
+                </LayoutProvider>
             </PrimeReactProvider>
         </>
     );
