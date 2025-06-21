aa880a69e3d00546cfa2587fe1c9bce5a37ca3f0
José Carlos Castillo Villa <DevJCCastilloVilla@outlook.es>
2025-06-21 01:44:10 -0600

    modificaciones menu

--- resources/js/Layouts/layout/AppMenu.jsx
+++ resources/js/Layouts/layout/AppMenu.jsx
@@ -20,7 +20,6 @@ const AppMenu = () => {
                     to: route("dashboard"),
                 },
                 // { label: 'Button', icon: 'pi pi-fw pi-id-card', to: route('button') },
-<<<<<<< HEAD
                 {
                     label: "Cotizaciones",
                     icon: "pi pi-fw pi-folder-open",
@@ -41,13 +40,6 @@ const AppMenu = () => {
                     ),
                 },
             ],
-=======
-                { label: 'Cotizaciones', icon: 'pi pi-fw pi-folder-open', to: route('cotizacion.show.index') },
-                { label: 'Actividades', icon: 'pi pi-fw pi-calendar-times', to: route('activiades.index') },
-                { label: 'Catalogos', icon: 'pi pi-fw pi-tags', to: route('catalogo.gral.index') },
-                
-            ]
->>>>>>> 52c059853619695da261950edfe6dab5127e7ad9
         },
     ];
 
