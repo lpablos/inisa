98220439d88c8bdd1ad909959e1580b34d24ec4f
Luis Jorge (MAC) <gevolutionxalapa@gmail.com>
2025-07-01 01:25:03 -0600

    Fix eliminacion de existencia de registros

--- resources/js/Pages/Actividades/Partials/ListadoActividades.jsx
+++ resources/js/Pages/Actividades/Partials/ListadoActividades.jsx
@@ -85,101 +85,109 @@ const ListadoActividades = ({nombre, registros = [] , paginaActual,perPage,total
     return (
 
         <div className="card">
-             <div className="grid m-1">                  
-                        <div className="col-12 text-right">                           
-                            <NuevaActividad usuario={nombre}/>                        
-                        </div>
-                    </div>
+            <div className="grid m-1">                  
+                <div className="col-12 text-right">                           
+                    <NuevaActividad usuario={nombre}/>                        
+                </div>
+            </div>
 
-          
-            <Accordion activeIndex={0}>
-                {tareasListado.map((tarea, index) => (
-                    <AccordionTab
-                        key={tarea.id}
-                        header={
-                            <span className="flex align-items-center gap-2 w-full">
-                                <span className="font-bold white-space-nowrap">
-                                    <strong>{tarea.titulo}</strong>
-                                </span>
-                                <Badge value={new Date(tarea.created_at).toLocaleDateString('es-MX')} className="ml-auto" />
-                            </span>
-                        }
-                    >
+            {tareasListado.length === 0 ? (
+                <div className="p-4 text-gray-500 text-center italic">
+                    No hay actividades registradas en el rango de fechas.
+                </div>
+            ) : ( 
+                <>
+                    <Accordion activeIndex={0}>
+                        
+                        {tareasListado.map((tarea, index) => (
+                            <AccordionTab
+                                key={tarea.id}
+                                header={
+                                    <span className="flex align-items-center gap-2 w-full">
+                                        <span className="font-bold white-space-nowrap">
+                                            <strong>{tarea.titulo}</strong>
+                                        </span>
+                                        <Badge value={new Date(tarea.created_at).toLocaleDateString('es-MX')} className="ml-auto" />
+                                    </span>
+                                }
+                            >
 
-                         <div className="flex flex-column gap-3">
-                            <div className="flex flex-wrap gap-3 mb-4">
-                                <div className="flex-auto min-w-[250px]">
-                                    <label htmlFor={`responsable-${index}`} className="font-bold block mb-2">Responsable</label>
-                                    <InputText id={`responsable-${index}`} value={tarea.usuario.name} className="w-full" placeholder="Nombre del responsable" readOnly={true}/>
+                                <div className="flex flex-column gap-3">
+                                    <div className="flex flex-wrap gap-3 mb-4">
+                                        <div className="flex-auto min-w-[250px]">
+                                            <label htmlFor={`responsable-${index}`} className="font-bold block mb-2">Responsable</label>
+                                            <InputText id={`responsable-${index}`} value={tarea.usuario.name} className="w-full" placeholder="Nombre del responsable" readOnly={true}/>
+                                        </div>
+
+                                        <div className="flex-auto min-w-[250px]">
+                                            <label htmlFor={`titulo-${index}`} className="font-bold block mb-2">Títutlo</label>
+                                            <InputText 
+                                                id={`titulo-${index}`} 
+                                                value={tarea.titulo} 
+                                                className="w-full" 
+                                                placeholder="Titulo"
+                                                onChange={(e) => manejoTitulo(e.value, index)}
+                                            />
+                                        </div>
+
+
+                                        <div className="flex-auto min-w-[250px]">
+                                            <label htmlFor={`prioridad-${index}`} className="font-bold block mb-2">Prioridad</label>
+                                            <Dropdown
+                                                inputId={`prioridad-${index}`}
+                                                value={prioridad.find(p => p.code === tarea.prioridad)}
+                                                onChange={(e) => manejoPrioridad(e.value, index)}
+                                                options={prioridad}
+                                                optionLabel="name"
+                                                placeholder="Selecciona Prioridad"
+                                                className="w-full"
+                                            />
+                                        </div>
+
+                                        <div className="flex-auto min-w-[250px]">
+                                            <label htmlFor={`estatus-${index}`} className="font-bold block mb-2">Estatus</label>
+                                            <Dropdown
+                                                inputId={`estatus-${index}`}                                        
+                                                value={estatus.find(p => p.code === tarea.estatus)}
+                                                onChange={(e) => manejoEstatus(e.value)}
+                                                options={estatus}
+                                                optionLabel="name"
+                                                placeholder="Selecciona Estatus"
+                                                className="w-full"
+                                            />
+                                        </div>
+
+                                        <div className="flex-auto min-w-[250px]">
+                                            <label htmlFor={`fecha-${index}`} className="font-bold block mb-2">Fecha Limite</label>
+                                            <Calendar 
+                                                inputId={`fecha-${index}`} 
+                                                value={tarea.fecha ? new Date(tarea.fecha) : null}
+                                                onChange={(e) => manejoFecha(e.value, index)} 
+                                                dateFormat="dd/mm/yy" 
+                                                className="w-full" 
+                                                showIcon/>
+                                        </div>
+                                    </div>
+                                    <Editor value={tarea.descripcion} onTextChange={(e) => manejoDescripcion(e.htmlValue, index)} style={{ height: '120px' }} />
+                                    <div className="flex items-center gap-3 mb-4 p-4">
+                                        <div className="p-inputgroup w-[500px]">
+                                            <Button icon="pi pi-search" label="Cotización" className="p-button-sm" />
+                                            <InputText placeholder="Vote" value="052025/001" className="text-sm" />
+                                            <Button icon="pi pi-eye" label="Visualizar" className="p-button-sm" />
+                                        </div>
+
+                                        <div className="flex gap-2">
+                                            <Button label="Actualizar" className="p-button-sm" />
+                                            <Button label="Eliminar" className="p-button-sm p-button-danger" onClick={()=>manejoEliminarActividad(tarea.id, index)}/>
+                                        </div>
+                                    </div>
                                 </div>
-
-                                <div className="flex-auto min-w-[250px]">
-                                    <label htmlFor={`titulo-${index}`} className="font-bold block mb-2">Títutlo</label>
-                                    <InputText 
-                                        id={`titulo-${index}`} 
-                                        value={tarea.titulo} 
-                                        className="w-full" 
-                                        placeholder="Titulo"
-                                        onChange={(e) => manejoTitulo(e.value, index)}
-                                    />
-                                </div>
-
-
-                                <div className="flex-auto min-w-[250px]">
-                                    <label htmlFor={`prioridad-${index}`} className="font-bold block mb-2">Prioridad</label>
-                                     <Dropdown
-                                        inputId={`prioridad-${index}`}
-                                        value={prioridad.find(p => p.code === tarea.prioridad)}
-                                        onChange={(e) => manejoPrioridad(e.value, index)}
-                                        options={prioridad}
-                                        optionLabel="name"
-                                        placeholder="Selecciona Prioridad"
-                                        className="w-full"
-                                    />
-                                </div>
-
-                                <div className="flex-auto min-w-[250px]">
-                                    <label htmlFor={`estatus-${index}`} className="font-bold block mb-2">Estatus</label>
-                                    <Dropdown
-                                        inputId={`estatus-${index}`}                                        
-                                        value={estatus.find(p => p.code === tarea.estatus)}
-                                        onChange={(e) => manejoEstatus(e.value)}
-                                        options={estatus}
-                                        optionLabel="name"
-                                        placeholder="Selecciona Estatus"
-                                        className="w-full"
-                                    />
-                                </div>
-
-                                <div className="flex-auto min-w-[250px]">
-                                    <label htmlFor={`fecha-${index}`} className="font-bold block mb-2">Fecha Limite</label>
-                                    <Calendar 
-                                        inputId={`fecha-${index}`} 
-                                        value={tarea.fecha ? new Date(tarea.fecha) : null}
-                                        onChange={(e) => manejoFecha(e.value, index)} 
-                                        dateFormat="dd/mm/yy" 
-                                        className="w-full" 
-                                        showIcon/>
-                                </div>
-                            </div>
-                            <Editor value={tarea.descripcion} onTextChange={(e) => manejoDescripcion(e.htmlValue, index)} style={{ height: '120px' }} />
-                            <div className="flex items-center gap-3 mb-4 p-4">
-                                <div className="p-inputgroup w-[500px]">
-                                    <Button icon="pi pi-search" label="Cotización" className="p-button-sm" />
-                                    <InputText placeholder="Vote" value="052025/001" className="text-sm" />
-                                    <Button icon="pi pi-eye" label="Visualizar" className="p-button-sm" />
-                                </div>
-
-                                <div className="flex gap-2">
-                                    <Button label="Actualizar" className="p-button-sm" />
-                                    <Button label="Eliminar" className="p-button-sm p-button-danger" onClick={()=>manejoEliminarActividad(tarea.id, index)}/>
-                                </div>
-                            </div>
-                        </div>
-                    </AccordionTab> 
-                ))}
-               
-            </Accordion>
+                            </AccordionTab> 
+                        ))}
+                    
+                    </Accordion>
+                </>
+            )}
             <EliminarMotivo activarEliminar={preguntaEliminar} setPreguntaEliminar={setPreguntaEliminar} identyDelete={identyDelete} setIdentyDelete={setIdentyDelete} recargar={recargar}/>
             <Paginator
                 first={first}
@@ -188,7 +196,7 @@ const ListadoActividades = ({nombre, registros = [] , paginaActual,perPage,total
                 onPageChange={handlePageChange}
                 rowsPerPageOptions={[20]}
             />
-            {/* <Paginator first={first} rows={rows} totalRecords={120}  onPageChange={onPageChange} /> */}
+            
         
         </div>
     );
